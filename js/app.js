7929a81bbf7f4c08d13ba20418df6fb74a8ebdf1
AraceliCumen <aycuevam@gmail.com>
2017-11-14 15:58:19 -0500

    Subiendo Poryecto de Office Hours

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+var parrafo = document.body.firstElementChild;
+console.log(parrafo);
+
+var image = document.body.children[1].firstElementChild;
+console.log(image);
+
+var textParrafo = document.body.firstElementChild.firstChild;
+console.log(textParrafo);
+
+var parrafoNuevo = document.getElementById('parrafo');
+console.log(parrafoNuevo);
+
+var imageNuevo= document.getElementsByTagName('img')[0];
+console.log(imageNuevo);
+
+// Accediendo a mi primer Hobiee
+var oneHobiee= document.querySelector('li');
+console.log(oneHobiee);
+
+// Accediendo a mi 2do Hobiee
+var twoHobiee=document.querySelectorAll('li')[1].
